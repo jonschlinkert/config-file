8e09cbaa2206bc118947cf182f135b6c96f00340
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-03 01:16:02 -0500

    remove chai

--- test/test.js
+++ test/test.js
@@ -1,55 +1,56 @@
-const expect = require('chai').expect;
-const path = require('path');
-const file = require('fs-utils');
-const config = require('../');
+'use strict';
+
+var assert = require('assert');
+var path = require('path');
+var config = require('../');
 
 describe('local config:', function () {
   it('should parse and load package.json when no file is specified.', function () {
     var actual = config.load();
-    expect(actual.name).to.eql('config-file');
+    assert.deepEqual(actual.name, 'config-file');
   });
 
   it('should parse and load the specified config file and return JSON.', function () {
     var actual = config.load('test/fixtures/.configrc');
     var expected = {name: 'Config', description: 'A JSON config file'};
-    expect(actual).to.eql(expected);
+    assert.deepEqual(actual, expected);
   });
 
   it('should parse the specified YAML config file and return JSON.', function () {
     var actual = config.load('test/fixtures/.configrc.yml', {parse: 'yaml'});
     var expected = {name: 'Config', description: 'A YAML config file'};
-    expect(actual).to.eql(expected);
+    assert.deepEqual(actual, expected);
   });
 
   it('should return null when no config file is found', function () {
     var actual = config.load('test/fixtures/.nothingrc.yml');
-    expect(actual == null).to.be.ok;
+    assert.deepEqual(actual === null, true);
   });
 
   it('should parse and load package.json from a specified cwd', function () {
     var cwd = path.resolve(path.join(process.cwd(), 'node_modules', 'fs-utils'));
     var actual = config.load({cwd: cwd});
-    expect(actual.name).to.eql('fs-utils');
+    assert.deepEqual(actual.name, 'fs-utils');
   });
 
   it('should parse and load the specified config file and return JSON, given a specified cwd.', function () {
     var cwd = path.resolve(path.join(process.cwd(), 'test', 'fixtures'));
     var actual = config.load('.configrc', {cwd: cwd});
     var expected = {name: 'Config', description: 'A JSON config file'};
-    expect(actual).to.eql(expected);
+    assert.deepEqual(actual, expected);
   });
 
   it('should parse the specified YAML config file and return JSON, given a specified cwd.', function () {
     var cwd = path.resolve(path.join(process.cwd(), 'test', 'fixtures'));
     var actual = config.load('.configrc.yml', {parse: 'yaml', cwd: cwd});
     var expected = {name: 'Config', description: 'A YAML config file'};
-    expect(actual).to.eql(expected);
+    assert.deepEqual(actual, expected);
   });
 
   it('should return null when no config file is found', function () {
     var cwd = path.resolve(path.join(process.cwd(), 'test', 'fixtures'));
     var actual = config.load('.nothingrc.yml', {cwd: cwd});
-    expect(actual == null).to.be.ok;
+    assert.deepEqual(actual === null, true);
   });
 
 });
@@ -58,7 +59,7 @@ describe('npm config:', function () {
   describe('when a package in node_modules is defined:', function () {
     it('should parse the specified config file and return JSON.', function () {
       var actual = config.npmLoad('fs-utils', '.jshintrc');
-      var expected = {
+      assert.deepEqual(actual, {
         "boss": true,
         "curly": true,
         "eqeqeq": true,
@@ -72,18 +73,17 @@ describe('npm config:', function () {
         "sub": true,
         "undef": true,
         "unused": true
-      };
-      expect(actual).to.eql(expected);
+      });
     });
 
     it('should parse the specified config file and return JSON.', function () {
       var actual = config.npmLoad('fs-utils');
-      expect(actual.name).to.eql('fs-utils');
+      assert.deepEqual(actual.name, 'fs-utils');
     });
 
     it('should parse the specified config file and return JSON.', function () {
       var actual = config.npmLoad('fs-utils', 'bower.json');
-      expect(actual.name).to.eql('fs-utils');
+      assert.deepEqual(actual.name, 'fs-utils');
     });
   });
 
@@ -91,7 +91,7 @@ describe('npm config:', function () {
     it('should parse the specified config file and return JSON.', function () {
       var cwd = path.resolve(path.join(process.cwd(), 'node_modules', 'fs-utils'));
       var actual = config.npmLoad('globule', '.jshintrc', {cwd: cwd});
-      var expected = {
+      assert.deepEqual(actual, {
         "curly": true,
         "eqeqeq": true,
         "immed": true,
@@ -104,20 +104,19 @@ describe('npm config:', function () {
         "boss": true,
         "eqnull": true,
         "node": true
-      };
-      expect(actual).to.eql(expected);
+      });
     });
 
     it('should parse the specified config file and return JSON.', function () {
       var cwd = path.resolve(path.join(process.cwd(), 'node_modules', 'fs-utils'));
       var actual = config.npmLoad('globule', {cwd: cwd});
-      expect(actual.name).to.eql('globule');
+      assert.deepEqual(actual.name, 'globule');
     });
 
     it('should parse the specified config file and return JSON.', function () {
       var cwd = path.resolve(path.join(process.cwd(), 'node_modules', 'fs-utils'));
       var actual = config.npmLoad('js-yaml', 'bower.json', {cwd: cwd});
-      expect(actual.name).to.eql('js-yaml');
+      assert.deepEqual(actual.name, 'js-yaml');
     });
   });
 });
